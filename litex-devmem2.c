0cf35a8d9a8e683e125dd7a87bd2a058fa0be5db
Sean Cross <sean@xobs.io>
2018-09-10 17:24:13 +0800

    litex-devmem2: add support for direct connections
    This is now a fully-usable tool.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- litex-devmem2.c
+++ litex-devmem2.c
@@ -5,11 +5,17 @@
 
 #include "etherbone.h"
 
-#ifdef DEBUG
-#define DEBUG_RISCV(str, ...) fprintf(stderr, str, __VA_ARGS__)
-#else
-#define DEBUG_RISCV(str, ...)
-#endif
+static void print_help(const char *progname) {
+    printf("Usage: %s [-t|--target target] [-p|--port port]\n"
+           "                  [-a|--address address] [-v|--value value]\n"
+           "                  [-d|--direct]\n", progname);
+    printf("Target address defaults to 127.0.0.1, and port defaults to 1234.");
+    printf("Connects to a device over Etherbone or the LiteX bridge, and accesses Wishbone.\n");
+    printf("To connect directly over Etherbone without using litex_server, use --direct.\n");
+    printf("If --value is omitted, then a read is performed.  Otherwise, a write is performed.\n");
+
+    return;
+}
 
 int main(int argc, char **argv) {
 
@@ -17,7 +23,8 @@ int main(int argc, char **argv) {
     const char *host_address = "127.0.0.1";
     const char *host_port = "1234";
     int direct_connection = 0;
-    int is_write = 0;
+    int have_value = 0;
+    int have_address = 0;
     int c;
     uint32_t address = 0;
     uint32_t value = 0;
@@ -31,10 +38,11 @@ int main(int argc, char **argv) {
             {"target", required_argument, 0, 't'},
             {"port", required_argument, 0, 'p'},
             {"direct", no_argument, 0, 'd'},
+            {"help", no_argument, 0, 'h'},
             {0, 0, 0, 0},
         };
 
-        c = getopt_long(argc, argv, "h:p:d", long_options, &option_index);
+        c = getopt_long(argc, argv, "a:v:t:p:dh", long_options, &option_index);
         if (c == -1)
             break;
 
@@ -49,12 +57,13 @@ int main(int argc, char **argv) {
         case 'a':
             fprintf(stderr, "Setting host address: %s\n", optarg);
             address = strtoul(optarg, NULL, 0);
+            have_address = 1;
             break;
 
         case 'v':
             fprintf(stderr, "Setting value: %s\n", optarg);
             value = strtoul(optarg, NULL, 0);
-            is_write = 1;
+            have_value = 1;
             break;
 
         case 't':
@@ -71,20 +80,42 @@ int main(int argc, char **argv) {
             fprintf(stderr, "Setting direct connection\n");
             direct_connection = 1;
             break;
+
+        case 'h':
+            print_help(argv[0]);
+            return 0;
+
         default:
-            printf("?? getopt returned character code 0%o ??\n", c);
+            printf("Unrecognized option\n");
+            print_help(argv[0]);
             return 1;
-            break;
         }
     }
 
-    conn = eb_connect(host_address, host_port);
+    if (optind < argc) {
+        if (!have_address) {
+            address = strtoul(argv[optind++], NULL, 0);
+            have_address = 1;
+        }
+
+        if (!have_value && (optind < argc)) {
+            value = strtoul(argv[optind++], NULL, 0);
+            have_value = 1;
+        }
+    }
+
+    if (!have_address) {
+        printf("%s: Must specify an address\n", argv[0]);
+        return 1;
+    }
+
+    conn = eb_connect(host_address, host_port, direct_connection);
     if (!conn) {
         fprintf(stderr, "Unable to create connection\n");
         return 1;
     }
 
-    if (is_write) {
+    if (have_value) {
         uint32_t old_val = eb_read32(conn, address);
         eb_write32(conn, address, value);
         uint32_t new_val = eb_read32(conn, address);
