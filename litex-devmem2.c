5cc608f1227cce6f300427db68d2dd9d37e9e42e
Sean Cross <sean@xobs.io>
2018-09-10 16:49:50 +0800

    litex-devmem2: continue conversion to universal tool
    Signed-off-by: Sean Cross <sean@xobs.io>

--- litex-devmem2.c
+++ litex-devmem2.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <endian.h>
-#include <errno.h>
-#include <string.h>
 #include <stdlib.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-#include <sys/uio.h>
+#include <getopt.h>
+#include <string.h>
 
 #include "etherbone.h"
 
@@ -23,42 +13,87 @@
 
 int main(int argc, char **argv) {
 
-	struct eb_connection *conn;
-	uint32_t address;
-	uint32_t value;
-	int is_write = 0;
+    struct eb_connection *conn;
+    const char *host_address = "127.0.0.1";
+    const char *host_port = "1234";
+    int direct_connection = 0;
+    int is_write = 0;
+    int c;
+    uint32_t address = 0;
+    uint32_t value = 0;
 
-	conn = eb_connect("127.0.0.1", "1234");
-	if (!conn) {
-		fprintf(stderr, "Unable to create connection\n");
-		return 1;
-	}
-	if (argc == 1) {
-		fprintf(stderr, "Must specify an address\n");
-		return 1;
-	}
-	else if (argc == 2) {
-		address = strtoul(argv[1], NULL, 0);
-	}
-	else if (argc == 3) {
-		address = strtoul(argv[1], NULL, 0);
-		value = strtoul(argv[2], NULL, 0);
-		is_write = 1;
-	}
-	else if (argc == 4) {
-		fprintf(stderr, "not supported\n");
-	}
+    while (1) {
+        int this_option_optind = optind ? optind : 1;
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"address", required_argument, 0, 'a'},
+            {"value", required_argument, 0, 'v'},
+            {"target", required_argument, 0, 't'},
+            {"port", required_argument, 0, 'p'},
+            {"direct", no_argument, 0, 'd'},
+            {0, 0, 0, 0},
+        };
 
-	if (is_write) {
-		uint32_t old_val = eb_read32(conn, address);
-		eb_write32(conn, address, value);
-		uint32_t new_val = eb_read32(conn, address);
-		fprintf(stderr, "0x%08x 0x%08x -> 0x%08x (wanted: 0x%08x)\n", address, old_val, new_val, value);
-	}
-	else {
-		fprintf(stderr, "0x%08x: 0x%08x\n", address, eb_read32(conn, address));
-	}
+        c = getopt_long(argc, argv, "h:p:d", long_options, &option_index);
+        if (c == -1)
+            break;
 
-	eb_disconnect(&conn);
-	return 0;
+        switch (c) {
+        case 0:
+            printf("option %s", long_options[option_index].name);
+            if (optarg)
+                printf(" with arg %s", optarg);
+            printf("\n");
+            break;
+
+        case 'a':
+            fprintf(stderr, "Setting host address: %s\n", optarg);
+            address = strtoul(optarg, NULL, 0);
+            break;
+
+        case 'v':
+            fprintf(stderr, "Setting value: %s\n", optarg);
+            value = strtoul(optarg, NULL, 0);
+            is_write = 1;
+            break;
+
+        case 't':
+            fprintf(stderr, "Setting target address: %s\n", optarg);
+            host_address = strdup(optarg);
+            break;
+
+        case 'p':
+            fprintf(stderr, "Setting host port: %s\n", optarg);
+            host_port = strdup(optarg);
+            break;
+
+        case 'd':
+            fprintf(stderr, "Setting direct connection\n");
+            direct_connection = 1;
+            break;
+        default:
+            printf("?? getopt returned character code 0%o ??\n", c);
+            return 1;
+            break;
+        }
+    }
+
+    conn = eb_connect(host_address, host_port);
+    if (!conn) {
+        fprintf(stderr, "Unable to create connection\n");
+        return 1;
+    }
+
+    if (is_write) {
+        uint32_t old_val = eb_read32(conn, address);
+        eb_write32(conn, address, value);
+        uint32_t new_val = eb_read32(conn, address);
+        fprintf(stderr, "0x%08x 0x%08x -> 0x%08x (wanted: 0x%08x)\n", address, old_val, new_val, value);
+    }
+    else {
+        fprintf(stderr, "0x%08x: 0x%08x\n", address, eb_read32(conn, address));
+    }
+
+    eb_disconnect(&conn);
+    return 0;
 }
