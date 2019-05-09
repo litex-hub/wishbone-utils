7372b9dc4a0331657328a73a8d167fddb8ff4e65
Sean Cross <sean@xobs.io>
2019-05-09 13:11:48 +0800

    riscv: programmatically generate register list
    We now have all known CSRs for RISC-V.  Programmatically generate the
    XML.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- usb/src/Riscv.rs
+++ usb/src/Riscv.rs
@@ -1,6 +1,3 @@
-pub struct RiscvCpu {
-
-}
 
 #[derive(Debug)]
 pub enum RiscvCpuError {
@@ -8,101 +5,240 @@ pub enum RiscvCpuError {
     UnrecognizedFile(String /* requested filename */)
 }
 
-const TARGET_XML: &str = r#"<?xml version="1.0"?>
-<!DOCTYPE target SYSTEM "gdb-target.dtd">
-<target version="1.0">
-<feature name="org.gnu.gdb.riscv.cpu">
-<reg name="x0" bitsize="32" regnum="0" save-restore="no" type="int" group="general"/>
-<reg name="x1" bitsize="32" regnum="1" save-restore="no" type="int" group="general"/>
-<reg name="x2" bitsize="32" regnum="2" save-restore="no" type="int" group="general"/>
-<reg name="x3" bitsize="32" regnum="3" save-restore="no" type="int" group="general"/>
-<reg name="x4" bitsize="32" regnum="4" save-restore="no" type="int" group="general"/>
-<reg name="x5" bitsize="32" regnum="5" save-restore="no" type="int" group="general"/>
-<reg name="x6" bitsize="32" regnum="6" save-restore="no" type="int" group="general"/>
-<reg name="x7" bitsize="32" regnum="7" save-restore="no" type="int" group="general"/>
-<reg name="x8" bitsize="32" regnum="8" save-restore="no" type="int" group="general"/>
-<reg name="x9" bitsize="32" regnum="9" save-restore="no" type="int" group="general"/>
-<reg name="x10" bitsize="32" regnum="10" save-restore="no" type="int" group="general"/>
-<reg name="x11" bitsize="32" regnum="11" save-restore="no" type="int" group="general"/>
-<reg name="x12" bitsize="32" regnum="12" save-restore="no" type="int" group="general"/>
-<reg name="x13" bitsize="32" regnum="13" save-restore="no" type="int" group="general"/>
-<reg name="x14" bitsize="32" regnum="14" save-restore="no" type="int" group="general"/>
-<reg name="x15" bitsize="32" regnum="15" save-restore="no" type="int" group="general"/>
-<reg name="x16" bitsize="32" regnum="16" save-restore="no" type="int" group="general"/>
-<reg name="x17" bitsize="32" regnum="17" save-restore="no" type="int" group="general"/>
-<reg name="x18" bitsize="32" regnum="18" save-restore="no" type="int" group="general"/>
-<reg name="x19" bitsize="32" regnum="19" save-restore="no" type="int" group="general"/>
-<reg name="x20" bitsize="32" regnum="20" save-restore="no" type="int" group="general"/>
-<reg name="x21" bitsize="32" regnum="21" save-restore="no" type="int" group="general"/>
-<reg name="x22" bitsize="32" regnum="22" save-restore="no" type="int" group="general"/>
-<reg name="x23" bitsize="32" regnum="23" save-restore="no" type="int" group="general"/>
-<reg name="x24" bitsize="32" regnum="24" save-restore="no" type="int" group="general"/>
-<reg name="x25" bitsize="32" regnum="25" save-restore="no" type="int" group="general"/>
-<reg name="x26" bitsize="32" regnum="26" save-restore="no" type="int" group="general"/>
-<reg name="x27" bitsize="32" regnum="27" save-restore="no" type="int" group="general"/>
-<reg name="x28" bitsize="32" regnum="28" save-restore="no" type="int" group="general"/>
-<reg name="x29" bitsize="32" regnum="29" save-restore="no" type="int" group="general"/>
-<reg name="x30" bitsize="32" regnum="30" save-restore="no" type="int" group="general"/>
-<reg name="x31" bitsize="32" regnum="31" save-restore="no" type="int" group="general"/>
-<reg name="pc" bitsize="32" regnum="32" save-restore="no" type="int" group="general"/>
-</feature>
-<feature name="org.gnu.gdb.riscv.csr">
-<reg name="ustatus" bitsize="32" regnum="65" save-restore="no" type="int" group="csr"/>
-<reg name="fflags" bitsize="32" regnum="66" save-restore="no" type="int" group="csr"/>
-<reg name="frm" bitsize="32" regnum="67" save-restore="no" type="int" group="csr"/>
-<reg name="fcsr" bitsize="32" regnum="68" save-restore="no" type="int" group="csr"/>
-<reg name="uie" bitsize="32" regnum="69" save-restore="no" type="int" group="csr"/>
-<reg name="utvec" bitsize="32" regnum="70" save-restore="no" type="int" group="csr"/>
-<reg name="uscratch" bitsize="32" regnum="129" save-restore="no" type="int" group="csr"/>
-<reg name="uepc" bitsize="32" regnum="130" save-restore="no" type="int" group="csr"/>
-<reg name="ucause" bitsize="32" regnum="131" save-restore="no" type="int" group="csr"/>
-<reg name="utval" bitsize="32" regnum="132" save-restore="no" type="int" group="csr"/>
-<reg name="uip" bitsize="32" regnum="133" save-restore="no" type="int" group="csr"/>
-<reg name="mstatus" bitsize="32" regnum="833" save-restore="no" type="int" group="csr"/>
-<reg name="misa" bitsize="32" regnum="834" save-restore="no" type="int" group="csr"/>
-<reg name="medeleg" bitsize="32" regnum="835" save-restore="no" type="int" group="csr"/>
-<reg name="mideleg" bitsize="32" regnum="836" save-restore="no" type="int" group="csr"/>
-<reg name="mie" bitsize="32" regnum="837" save-restore="no" type="int" group="csr"/>
-<reg name="mtvec" bitsize="32" regnum="838" save-restore="no" type="int" group="csr"/>
-<reg name="mcounteren" bitsize="32" regnum="839" save-restore="no" type="int" group="csr"/>
-<reg name="mscratch" bitsize="32" regnum="897" save-restore="no" type="int" group="csr"/>
-<reg name="mepc" bitsize="32" regnum="898" save-restore="no" type="int" group="csr"/>
-<reg name="mcause" bitsize="32" regnum="899" save-restore="no" type="int" group="csr"/>
-<reg name="mtval" bitsize="32" regnum="900" save-restore="no" type="int" group="csr"/>
-<reg name="mip" bitsize="32" regnum="901" save-restore="no" type="int" group="csr"/>
-<reg name="mtohost" bitsize="32" regnum="1985" save-restore="no" type="int" group="csr"/>
-<reg name="mfromhost" bitsize="32" regnum="1986" save-restore="no" type="int" group="csr"/>
-<reg name="mreset" bitsize="32" regnum="1987" save-restore="no" type="int" group="csr"/>
-<reg name="mipi" bitsize="32" regnum="1988" save-restore="no" type="int" group="csr"/>
-<reg name="miobase" bitsize="32" regnum="1989" save-restore="no" type="int" group="csr"/>
-<reg name="cycle" bitsize="32" regnum="3137" save-restore="no" type="int" group="csr"/>
-<reg name="time" bitsize="32" regnum="3138" save-restore="no" type="int" group="csr"/>
-<reg name="instret" bitsize="32" regnum="3139" save-restore="no" type="int" group="csr"/>
-<reg name="cycleh" bitsize="32" regnum="3265" save-restore="no" type="int" group="csr"/>
-<reg name="timeh" bitsize="32" regnum="3266" save-restore="no" type="int" group="csr"/>
-<reg name="instreth" bitsize="32" regnum="3267" save-restore="no" type="int" group="csr"/>
-<reg name="mvendorid" bitsize="32" regnum="3922" save-restore="no" type="int" group="csr"/>
-<reg name="marchid" bitsize="32" regnum="3923" save-restore="no" type="int" group="csr"/>
-<reg name="mimpid" bitsize="32" regnum="3924" save-restore="no" type="int" group="csr"/>
-</feature>
-</target>
-"#;
-
 const THREADS_XML: &str = r#"<?xml version="1.0"?>
 <threads>
 </threads>"#;
 
+#[derive(PartialEq)]
+enum RiscvRegisterType {
+    /// Normal CPU registers
+    General,
+
+    /// Arch-specific registers
+    CSR,
+}
+
+impl RiscvRegisterType {
+    fn feature_name(&self) -> &str {
+        match *self {
+            RiscvRegisterType::General => "org.gnu.gdb.riscv.cpu",
+            RiscvRegisterType::CSR => "org.gnu.gdb.riscv.csr",
+        }
+    }
+
+    fn group(&self) -> &str {
+        match *self {
+            RiscvRegisterType::General => "general",
+            RiscvRegisterType::CSR => "csr",
+        }
+    }
+}
+
+struct RiscvRegister {
+
+    /// Which register group this belongs to
+    register_type: RiscvRegisterType,
+
+    /// Index within its namespace (e.g. `ustatus` is a CSR with index 0x000,
+    /// even though GDB registers are offset by 65, so GDB calls `ustatus` register 65.)
+    index: u32,
+
+    /// Architecture name
+    name: String,
+
+    /// Whether this register is present on this device
+    present: bool,
+}
+
+impl RiscvRegister {
+    pub fn general(index: u32, name: &str) -> RiscvRegister {
+        RiscvRegister {
+            register_type: RiscvRegisterType::General,
+            index,
+            name: name.to_string(),
+            present: true,
+        }
+    }
+
+    pub fn csr(index: u32, name: &str, present: bool) -> RiscvRegister {
+        RiscvRegister {
+            register_type: RiscvRegisterType::CSR,
+            index,
+            name: name.to_string(),
+            present,
+        }
+    }
+}
+
+pub struct RiscvCpu {
+
+    /// A list of all available registers on this CPU
+    registers: Vec<RiscvRegister>,
+
+    /// An XML representation of the register mapping
+    target_xml: String,
+}
+
 impl RiscvCpu {
     pub fn new() -> Result<RiscvCpu, RiscvCpuError> {
-        Ok(RiscvCpu {})
+        let registers = Self::make_registers();
+        let target_xml = Self::make_target_xml(&registers);
+        Ok(RiscvCpu {registers, target_xml})
+    }
+
+    fn make_registers() -> Vec<RiscvRegister> {
+        let mut registers = vec![];
+
+        // Add in general purpose registers x0 to x31
+        for reg_num in 0..32 {
+            registers.push(RiscvRegister::general(reg_num, &format!("x{}", reg_num)));
+        }
+
+        // Add the program counter
+        registers.push(RiscvRegister::general(32, "pc"));
+
+        // User trap setup
+        registers.push(RiscvRegister::csr(0x000, "ustatus", false));
+        registers.push(RiscvRegister::csr(0x004, "uie", false));
+        registers.push(RiscvRegister::csr(0x005, "utvec", false));
+
+        // User trap handling
+        registers.push(RiscvRegister::csr(0x040, "uscratch", false));
+        registers.push(RiscvRegister::csr(0x041, "uepc", false));
+        registers.push(RiscvRegister::csr(0x042, "ucause", false));
+        registers.push(RiscvRegister::csr(0x043, "utval", false));
+        registers.push(RiscvRegister::csr(0x044, "uip", false));
+
+        // User counter/timers
+        registers.push(RiscvRegister::csr(0xc00, "cycle", false));
+        registers.push(RiscvRegister::csr(0xc01, "time", false));
+        registers.push(RiscvRegister::csr(0xc02, "instret", false));
+        for hpmcounter_n in 3..32 {
+            registers.push(RiscvRegister::csr(0xc00 + hpmcounter_n, &format!("hpmcounter{}", hpmcounter_n), false));
+        }
+        registers.push(RiscvRegister::csr(0xc80, "cycleh", false));
+        registers.push(RiscvRegister::csr(0xc81, "timeh", false));
+        registers.push(RiscvRegister::csr(0xc82, "instreth", false));
+        for hpmcounter_n in 3..32 {
+            registers.push(RiscvRegister::csr(0xc80 + hpmcounter_n, &format!("hpmcounter{}h", hpmcounter_n), false));
+        }
+
+        // Supervisor Trap Setup
+        registers.push(RiscvRegister::csr(0x100, "sstatus", false));
+        registers.push(RiscvRegister::csr(0x102, "sedeleg", false));
+        registers.push(RiscvRegister::csr(0x103, "sideleg", false));
+        registers.push(RiscvRegister::csr(0x104, "sie", false));
+        registers.push(RiscvRegister::csr(0x105, "stvec", false));
+        registers.push(RiscvRegister::csr(0x106, "scounteren", false));
+
+        // Supervisor Trap Handling
+        registers.push(RiscvRegister::csr(0x140, "sscratch", false));
+        registers.push(RiscvRegister::csr(0x141, "sepc", false));
+        registers.push(RiscvRegister::csr(0x142, "scause", false));
+        registers.push(RiscvRegister::csr(0x143, "stval", false));
+        registers.push(RiscvRegister::csr(0x144, "sip", false));
+
+        // Supervisor protection and translation
+        registers.push(RiscvRegister::csr(0x180, "satp", false));
+
+        // Machine information registers
+        registers.push(RiscvRegister::csr(0xf11, "mvendorid", false));
+        registers.push(RiscvRegister::csr(0xf12, "marchid", false));
+        registers.push(RiscvRegister::csr(0xf13, "mimpid", false));
+        registers.push(RiscvRegister::csr(0xf14, "mhartid", false));
+
+        // Machine trap setup
+        registers.push(RiscvRegister::csr(0x300, "mstatus", false));
+        registers.push(RiscvRegister::csr(0x301, "misa", false));
+        registers.push(RiscvRegister::csr(0x302, "medeleg", false));
+        registers.push(RiscvRegister::csr(0x303, "mideleg", false));
+        registers.push(RiscvRegister::csr(0x304, "mie", false));
+        registers.push(RiscvRegister::csr(0x305, "mtvec", false));
+        registers.push(RiscvRegister::csr(0x306, "mcounteren", false));
+
+        // Machine trap handling
+        registers.push(RiscvRegister::csr(0x340, "mscratch", false));
+        registers.push(RiscvRegister::csr(0x341, "mepc", false));
+        registers.push(RiscvRegister::csr(0x342, "mcause", false));
+        registers.push(RiscvRegister::csr(0x343, "mtval", false));
+        registers.push(RiscvRegister::csr(0x344, "mip", false));
+
+        // Machine protection and translation
+        registers.push(RiscvRegister::csr(0x3a0, "mpmcfg0", false));
+        registers.push(RiscvRegister::csr(0x3a1, "mpmcfg1", false));
+        registers.push(RiscvRegister::csr(0x3a2, "mpmcfg2", false));
+        registers.push(RiscvRegister::csr(0x3a3, "mpmcfg3", false));
+        for pmpaddr_n in 0..16 {
+            registers.push(RiscvRegister::csr(0x3b0 + pmpaddr_n, &format!("pmpaddr{}", pmpaddr_n), false));
+        }
+
+        // Machine counter/timers
+        registers.push(RiscvRegister::csr(0xb00, "mcycle", false));
+        registers.push(RiscvRegister::csr(0xb02, "minstret", false));
+        for mhpmcounter_n in 3..32 {
+            registers.push(RiscvRegister::csr(0xb00 + mhpmcounter_n, &format!("mhpmcounter{}", mhpmcounter_n), false));
+        }
+        registers.push(RiscvRegister::csr(0xb80, "mcycleh", false));
+        registers.push(RiscvRegister::csr(0xb82, "minstreth", false));
+        for mhpmcounter_n in 3..32 {
+            registers.push(RiscvRegister::csr(0xb80 + mhpmcounter_n, &format!("mhpmcounter{}h", mhpmcounter_n), false));
+        }
+
+        // Machine counter setup
+        for mhpmevent_n in 3..32 {
+            registers.push(RiscvRegister::csr(0x320 + mhpmevent_n, &format!("mhpmevent{}", mhpmevent_n), false));
+        }
+
+        // Debug/trace registers
+        registers.push(RiscvRegister::csr(0x7a0, "tselect", false));
+        registers.push(RiscvRegister::csr(0x7a1, "tdata1", false));
+        registers.push(RiscvRegister::csr(0x7a2, "tdata2", false));
+        registers.push(RiscvRegister::csr(0x7a3, "tdata3", false));
+
+        // Debug mode registers
+        registers.push(RiscvRegister::csr(0x7b0, "dcsr", false));
+        registers.push(RiscvRegister::csr(0x7b1, "dpc", false));
+        registers.push(RiscvRegister::csr(0x7b2, "dscratch", false));
+
+        registers
+    }
+
+    fn make_target_xml(registers: &Vec<RiscvRegister>) -> String {
+        let mut target_xml = r#"
+            <?xml version=\"1.0\"?>
+                <!DOCTYPE target SYSTEM "gdb-target.dtd">
+                <target version="1.0">
+        "#.to_string();
+
+        // Add in general-purpose registers
+        for ft in &[RiscvRegisterType::General, RiscvRegisterType::CSR] {
+            target_xml.push_str(&format!("<feature name=\"{}\">\n", ft.feature_name()));
+            for reg in registers {
+                if ! reg.present || reg.register_type != *ft {
+                    continue;
+                }
+                target_xml.push_str(
+                    &format!("<reg name=\"{}\" bitsize=\"32\" regnum=\"{}\" save-restore=\"no\" type=\"int\" group=\"{}\"/>\n",
+                        reg.name, reg.index, reg.register_type.group())
+                );
+            }
+        }
+        target_xml.push_str("</feature>\n");
+        target_xml.push_str("</target>\n");
+
+        target_xml
     }
 
     pub fn get_feature(&self, name: &str) -> Result<Vec<u8>, RiscvCpuError> {
         if name == "target.xml" {
-            let xml = TARGET_XML.to_string().into_bytes();
+            let xml = self.target_xml.to_string().into_bytes();
             Ok(xml)
         } else {
             Err(RiscvCpuError::UnrecognizedFile(name.to_string()))
         }
+    }
+
+    pub fn get_threads(&self) -> Result<Vec<u8>, RiscvCpuError> {
+        Ok(THREADS_XML.to_string().into_bytes())
     }
 }
