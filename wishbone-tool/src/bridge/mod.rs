d2dca77efd68a4b352de0ed75de3b40c7cd616d9
Sean Cross <sean@xobs.io>
2019-09-03 15:44:15 +0800

    bridge: wrap bridge in a mutex
    This prevents some errors we were seeing where values were getting
    crossed.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- wishbone-tool/src/bridge/mod.rs
+++ wishbone-tool/src/bridge/mod.rs
@@ -17,10 +17,16 @@ pub enum BridgeKind {
 }
 
 #[derive(Clone)]
-pub enum Bridge {
+pub enum BridgeCore {
     UsbBridge(UsbBridge),
     UartBridge(UartBridge),
     SpiBridge(SpiBridge),
+}
+
+#[derive(Clone)]
+pub struct Bridge {
+    core: BridgeCore,
+    mutex: Arc<Mutex<()>>,
 }
 
 #[derive(Debug)]
@@ -59,44 +65,56 @@ impl std::convert::From<io::Error> for BridgeError {
 
 impl Bridge {
     pub fn new(cfg: &Config) -> Result<Bridge, BridgeError> {
+        let mutex = Arc::new(Mutex::new(()));
         match cfg.bridge_kind {
-            BridgeKind::UartBridge => Ok(Bridge::UartBridge(UartBridge::new(cfg)?)),
-            BridgeKind::UsbBridge => Ok(Bridge::UsbBridge(UsbBridge::new(cfg)?)),
-            BridgeKind::SpiBridge => Ok(Bridge::SpiBridge(SpiBridge::new(cfg)?)),
+            BridgeKind::UartBridge => Ok(Bridge { mutex, core: BridgeCore::UartBridge(UartBridge::new(cfg)?) } ),
+            BridgeKind::UsbBridge => Ok(Bridge { mutex, core: BridgeCore::UsbBridge(UsbBridge::new(cfg)?) } ),
+            BridgeKind::SpiBridge => Ok(Bridge { mutex, core: BridgeCore::SpiBridge(SpiBridge::new(cfg)?) } ),
         }
     }
 
     pub fn connect(&self) -> Result<(), BridgeError> {
-        match self {
-            Bridge::UsbBridge(b) => b.connect(),
-            Bridge::UartBridge(b) => b.connect(),
-            Bridge::SpiBridge(b) => b.connect(),
+        let _mtx = self.mutex.lock().unwrap();
+        match &self.core{
+            BridgeCore::UsbBridge(b) => b.connect(),
+            BridgeCore::UartBridge(b) => b.connect(),
+            BridgeCore::SpiBridge(b) => b.connect(),
         }
     }
 
     pub fn mutex(&self) -> &Arc<Mutex<()>> {
-        match self {
-            Bridge::UsbBridge(b) => b.mutex(),
-            Bridge::UartBridge(b) => b.mutex(),
-            Bridge::SpiBridge(b) => b.mutex(),
+        match &self.core {
+            BridgeCore::UsbBridge(b) => b.mutex(),
+            BridgeCore::UartBridge(b) => b.mutex(),
+            BridgeCore::SpiBridge(b) => b.mutex(),
         }
     }
 
     pub fn peek(&self, addr: u32) -> Result<u32, BridgeError> {
-        let result = match self {
-            Bridge::UsbBridge(b) => b.peek(addr),
-            Bridge::UartBridge(b) => b.peek(addr),
-            Bridge::SpiBridge(b) => b.peek(addr),
-        };
-        result
+        let _mtx = self.mutex.lock().unwrap();
+        loop {
+            let result = match &self.core {
+                BridgeCore::UsbBridge(b) => b.peek(addr),
+                BridgeCore::UartBridge(b) => b.peek(addr),
+                BridgeCore::SpiBridge(b) => b.peek(addr),
+            };
+            if result.is_ok() {
+                return result;
+            }
+        }
     }
 
     pub fn poke(&self, addr: u32, value: u32) -> Result<(), BridgeError> {
-        let result = match self {
-            Bridge::UsbBridge(b) => b.poke(addr, value),
-            Bridge::UartBridge(b) => b.poke(addr, value),
-            Bridge::SpiBridge(b) => b.poke(addr, value),
-        };
-        result
+        let _mtx = self.mutex.lock().unwrap();
+        loop {
+            let result = match &self.core {
+                BridgeCore::UsbBridge(b) => b.poke(addr, value),
+                BridgeCore::UartBridge(b) => b.poke(addr, value),
+                BridgeCore::SpiBridge(b) => b.poke(addr, value),
+            };
+            if result.is_ok() {
+                return result;
+            }
+        }
     }
 }
