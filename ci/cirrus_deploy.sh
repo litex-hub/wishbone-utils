2594cb8e71455f10a622209c18b79410046c4ceb
Sean Cross <sean@xobs.io>
2020-01-07 12:35:07 +0800

    ci: cirrus: fix deploy target
    Signed-off-by: Sean Cross <sean@xobs.io>

--- ci/cirrus_deploy.sh
+++ ci/cirrus_deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 if [[ "$CIRRUS_TAG" == "" ]]; then
   echo "Not a tag. No need to deploy!"
@@ -7,6 +8,13 @@ fi
 
 if [[ "$GITHUB_TOKEN" == "" ]]; then
   echo "Please provide GitHub access token via GITHUB_TOKEN environment variable!"
+  exit 1
+fi
+
+# Convert the tag to a Github ID
+$release_id=$(curl -sH "$GITHUB_TOKEN" https://api.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/tags/$CIRRUS_TAG | grep -m 1 "id.:" | grep -w id | tr -cd '[0-9]')
+if [[ "$release_id" == "" ]]; then
+  ech "Unable to get release ID from tag $CIRRUS_TAG"
   exit 1
 fi
 
@@ -20,7 +28,7 @@ for fpath in $files_to_upload
 do
   echo "Uploading $fpath..."
   name=$(basename "$fpath")
-  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_TAG/assets?name=$name"
+  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$release_id/assets?name=$name"
   curl -X POST \
     --data-binary @$fpath \
     --header "Authorization: token $GITHUB_TOKEN" \
