c0766ba9720474f0541933fa18f6faece11c3d99
Sean Cross <sean@xobs.io>
2020-01-07 09:50:01 +0800

    ci: cirrus: replace CIRRUS_RELEASE with CIRRUS_TAG
    The documentation doesn't say how to create releases, so let's just use
    tags instead.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- ci/cirrus_deploy.sh
+++ ci/cirrus_deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ "$CIRRUS_RELEASE" == "" ]]; then
-  echo "Not a release. No need to deploy!"
+if [[ "$CIRRUS_TAG" == "" ]]; then
+  echo "Not a tag. No need to deploy!"
   exit 0
 fi
 
@@ -20,7 +20,7 @@ for fpath in $files_to_upload
 do
   echo "Uploading $fpath..."
   name=$(basename "$fpath")
-  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
+  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_TAG/assets?name=$name"
   curl -X POST \
     --data-binary @$fpath \
     --header "Authorization: token $GITHUB_TOKEN" \
