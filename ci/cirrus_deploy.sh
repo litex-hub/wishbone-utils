670fbcf0c6ba30924747386d929558eba7f5d247
Sean Cross <sean@xobs.io>
2020-01-07 18:02:08 +0800

    ci: cirrus: print out url on post
    Signed-off-by: Sean Cross <sean@xobs.io>

--- ci/cirrus_deploy.sh
+++ ci/cirrus_deploy.sh
@@ -22,7 +22,7 @@ fi
 # one artifact first.
 # However, Travis builds generally finish in 3-4 minutes, wherease Cirrus builds
 # take 7-10 minutes, so this usually isn't an issue.
-release_id=$(curl --fail https://api.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/tags/$CIRRUS_TAG | grep -m 1 "id.:" | grep -w id | tr -cd '[0-9]')
+release_id=$(curl --fail --header "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/tags/$CIRRUS_TAG | grep -m 1 "id.:" | grep -w id | tr -cd '[0-9]')
 if [[ "$release_id" == "" ]]; then
   echo "Unable to get release ID from tag $CIRRUS_TAG"
   exit 1
@@ -36,10 +36,10 @@ files_to_upload=(
 
 for fpath in $files_to_upload
 do
-  echo "Uploading $fpath..."
   name=$(basename "$fpath")
   url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$release_id/assets?name=$name"
-  curl --fail -X POST \
+  echo "Uploading $fpath to $url_to_upload"
+  curl -X POST \
     --data-binary @$fpath \
     --header "Authorization: token $GITHUB_TOKEN" \
     --header "Content-Type: $file_content_type" \
