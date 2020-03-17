42b4c13fee78185ebd3117803002afddf8078471
Sean Cross <sean@xobs.io>
2020-03-17 16:32:27 +0800

    bridge: usb: fix error when crashing at startup
    When we make a connection and immediately quit, there can be a flag left
    in the pipeline.  Ensure we drain the flag.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- wishbone-tool/src/bridge/usb.rs
+++ wishbone-tool/src/bridge/usb.rs
@@ -123,7 +123,7 @@ impl UsbBridge {
             while _mtx.is_none() {
                 _mtx = cvar.wait(_mtx).unwrap();
             }
-            match *_mtx {
+            match _mtx.take() {
                 Some(ConnectThreadResponses::OpenedDevice) => return Ok(()),
                 _ => (),
             }
