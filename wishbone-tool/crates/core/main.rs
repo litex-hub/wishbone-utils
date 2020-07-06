e4c24cd088accd3b0dddf9cf0f48a3cab60e1c7e
Sean Cross <sean@xobs.io>
2020-07-06 18:18:50 +0800

    wishbone-tool: clean up some command line help
    This fixes some of the confusion in the `--help` listing of the command
    line output.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- wishbone-tool/crates/core/main.rs
+++ wishbone-tool/crates/core/main.rs
@@ -24,27 +24,23 @@ fn clap_app<'a, 'b>() -> App<'a, 'b> {
         .about("Work with Wishbone devices over various bridges")
         .arg(
             Arg::with_name("completion")
+            .group("command")
                 .short("c")
                 .long("completion")
                 .help("Generate shell auto-completion file")
-                .required_unless("list")
-                .conflicts_with("list")
-                .required_unless("address")
-                .conflicts_with("address")
-                .required_unless("server-kind")
-                .conflicts_with("server-kind")
-                .display_order(3)
+                .display_order(1)
                 .possible_values(&Shell::variants())
                 .takes_value(true)
         )
+
         .arg(
             Arg::with_name("pid")
                 .short("p")
                 .long("pid")
                 .value_name("USB_PID")
-                .help("USB PID to match")
+                .help("USB: PID to match")
                 .default_value("0x5bf0")
-                .display_order(3)
+                .display_order(2)
                 .takes_value(true),
         )
         .arg(
@@ -52,8 +48,8 @@ fn clap_app<'a, 'b>() -> App<'a, 'b> {
                 .short("v")
                 .long("vid")
                 .value_name("USB_VID")
-                .help("USB VID to match")
-                .display_order(3)
+                .help("USB: VID to match")
+                .display_order(2)
                 .takes_value(true),
         )
         .arg(
@@ -61,8 +57,8 @@ fn clap_app<'a, 'b>() -> App<'a, 'b> {
                 .short("B")
                 .long("bus")
                 .value_name("USB_BUS")
-                .help("USB BUS to match")
-                .display_order(4)
+                .help("USB: bus to match")
+                .display_order(3)
                 .takes_value(true),
         )
         .arg(
@@ -70,17 +66,18 @@ fn clap_app<'a, 'b>() -> App<'a, 'b> {
                 .short("d")
                 .long("device")
                 .value_name("USB_DEVICE")
-                .help("USB DEVICE to match")
-                .display_order(4)
+                .help("USB: device to match")
+                .display_order(3)
                 .takes_value(true),
         )
+
         .arg(
             Arg::with_name("serial")
                 .short("u")
                 .long("serial")
                 .alias("uart")
                 .value_name("PORT")
-                .help("Serial port to use")
+                .help("SERIAL: path to serial port")
                 .display_order(4)
                 .takes_value(true),
         )
@@ -90,15 +87,16 @@ fn clap_app<'a, 'b>() -> App<'a, 'b> {
                 .long("baud")
                 .value_name("RATE")
                 .default_value("115200")
-                .help("Baudrate to use in serial mode")
+                .help("SERIAL: baudrate to use for serial port")
                 .display_order(5)
                 .takes_value(true),
         )
+
         .arg(
             Arg::with_name("ethernet-host")
                 .long("ethernet-host")
-                .value_name("HOSTNAME")
-                .help("Address to use when connecting via Etherbone")
+                .value_name("ADDRESS")
+                .help("ETHERNET: address of device or proxy to connect to")
                 .display_order(6)
                 .takes_value(true)
         )
@@ -106,43 +104,41 @@ fn clap_app<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("ethernet-port")
                 .long("ethernet-port")
                 .value_name("PORT")
-                .help("Port to use when connecting via Etherbone")
+                .help("ETHERNET: port to use for Ethernet bridge")
                 .default_value("1234")
-                .display_order(6)
+                .display_order(7)
                 .takes_value(true)
         )
         .arg(
             Arg::with_name("ethernet-tcp")
                 .long("ethernet-tcp")
-                .help("Connect using TCP, for example when using an external wishbone bridge")
-                .display_order(6)
+                .help("ETHERNET: use TCP to connect to Wishbone, such as when using a proxy")
+                .display_order(8)
         )
+
         .arg(
             Arg::with_name("pcie-bar")
                 .long("pcie-bar")
-                .help("Connect using PCIe using the specified BAR")
-                .display_order(6)
+                .help("PCIe: use the specified file as a PCIe BAR")
+                .display_order(9)
                 .takes_value(true)
         )
+
         .arg(
             Arg::with_name("spi-pins")
                 .short("g")
                 .long("spi-pins")
                 .value_delimiter("PINS")
-                .help("GPIO pins to use for COPI,CIPO,CLK,CS_N (e.g. 2,3,4,18)")
-                .display_order(6)
+                .help("SPI: GPIO pins to use for COPI,CIPO,CLK,CS_N (e.g. 2,3,4,18)")
+                .display_order(10)
                 .takes_value(true),
         )
+
         .arg(
             Arg::with_name("address")
                 .index(1)
-                .required_unless("completion")
-                .conflicts_with("completion")
-                .required_unless("server-kind")
-                .conflicts_with("server-kind")
-                .required_unless("list")
-                .conflicts_with("list")
-                .display_order(7)
+                .group("command")
+                .display_order(11)
                 .help("address to read/write"),
         )
         .arg(
@@ -150,17 +146,64 @@ fn clap_app<'a, 'b>() -> App<'a, 'b> {
                 .value_name("value")
                 .index(2)
                 .required(false)
-                .display_order(8)
+                .display_order(12)
                 .help("value to write"),
         )
+
+        .arg(
+            Arg::with_name("csr-csv")
+                .long("csr-csv")
+                .help("csr.csv file containing register mappings")
+                .display_order(13)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("register-offset")
+                .long("register-offset")
+                .alias("csr-csv-offset")
+                .help("apply an offset to addresses, e.g. to compensate for PCIe BAR offset")
+                .display_order(14)
+                .takes_value(true),
+        )
+
+        .arg(
+            Arg::with_name("server-kind")
+                .short("s")
+                .group("command")
+                .long("server")
+                .alias("server-kind")
+                .takes_value(true)
+                .multiple(true)
+                .help("which server to run (if any)")
+                .display_order(15)
+                .possible_values(&["gdb", "wishbone", "random-test", "load-file", "terminal", "messible"]),
+        )
+
+        .arg(
+            Arg::with_name("gdb-port")
+                .long("gdb-port")
+                .help("GDB: port to listen on for GDB connections")
+                .default_value("3333")
+                .display_order(16)
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("debug-offset")
+                .long("debug-offset")
+                .help("GDB: address of the CPU's debug bridge")
+                .default_value("0xf00f0000")
+                .display_order(17)
+                .takes_value(true),
+        )
+
         .arg(
             Arg::with_name("bind-addr")
                 .short("a")
                 .long("bind-addr")
                 .value_name("IP_ADDRESS")
-                .help("IP address to bind to")
+                .help("WISHBONE: IP address to bind to when acting as a server")
                 .default_value("127.0.0.1")
-                .display_order(2)
+                .display_order(18)
                 .takes_value(true),
         )
         .arg(
@@ -169,105 +212,63 @@ fn clap_app<'a, 'b>() -> App<'a, 'b> {
                 .long("wishbone-port")
                 .alias("port")
                 .value_name("PORT_NUMBER")
-                .help("port number to listen on")
+                .help("WISHBONE: port number to listen on when acting as a server")
                 .default_value("1234")
-                .display_order(2)
+                .display_order(19)
                 .takes_value(true),
         )
+
         .arg(
-            Arg::with_name("server-kind")
-                .short("s")
-                .long("server")
-                .alias("server-kind")
-                .takes_value(true)
-                .multiple(true)
-                .required_unless("completion")
-                .conflicts_with("completion")
-                .required_unless("address")
-                .conflicts_with("address")
-                .required_unless("list")
-                .conflicts_with("list")
-                .help("which server to run (if any)")
-                .display_order(1)
-                .possible_values(&["gdb", "wishbone", "random-test", "load-file", "terminal", "messible"]),
-        )
-        .arg(
-            Arg::with_name("gdb-port")
-                .long("gdb-port")
-                .help("Port to listen for GDB connections")
-                .default_value("3333")
-                .takes_value(true)
-        )
-        .arg(
-            Arg::with_name("load-name")
-                .long("load-name")
-                .help("A file to load into RAM")
-                .takes_value(true)
-                .display_order(13),
-        )
-        .arg(
-            Arg::with_name("load-address")
-                .long("load-address")
-                .help("Address for file to load")
-                .takes_value(true)
-                .display_order(13),
+            Arg::with_name("random-address")
+                .long("random-address")
+                .help("RANDOM_TEST: address at which to write")
+                .display_order(20)
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("random-loops")
                 .long("random-loops")
-                .help("number of loops to run when doing a random-test")
-                .display_order(9)
+                .help("RANDOM_TEST: Number of loops to run")
+                .display_order(21)
                 .takes_value(true),
         )
         .arg(
             Arg::with_name("random-range")
                 .long("random-range")
-                .help("the size of the random address range (i.e. how many bytes to randomly add to the address)")
-                .display_order(9)
+                .help("RANDOM_TEST: the size of the random address range (i.e. how many bytes to randomly add to the address)")
+                .display_order(22)
                 .takes_value(true),
+        )
+
+        .arg(
+            Arg::with_name("load-name")
+                .long("load-name")
+                .help("LOAD_FILE: Name of the file to load into RAM")
+                .takes_value(true)
+                .display_order(23),
         )
         .arg(
-            Arg::with_name("messible-address")
-                .long("messible-address")
-                .help("address to use to get messible messages from")
-                .display_order(9)
-                .takes_value(true),
+            Arg::with_name("load-address")
+                .long("load-address")
+                .help("LOAD_FILE: Address at which to load the file")
+                .takes_value(true)
+                .display_order(24),
         )
-        .arg(
-            Arg::with_name("csr-csv")
-                .long("csr-csv")
-                .help("csr.csv file containing register mappings")
-                .display_order(9)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("register-offset")
-                .long("register-offset")
-                .alias("csr-csv-offset")
-                .help("apply an offset to addresses, e.g. to specify PCIe BAR offset")
-                .display_order(9)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("random-address")
-                .long("random-address")
-                .help("address to write to when doing a random-test")
-                .display_order(10)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("debug-offset")
-                .long("debug-offset")
-                .help("address to use for debug bridge")
-                .default_value("0xf00f0000")
-                .display_order(11)
-                .takes_value(true),
-        )
+
         .arg(
             Arg::with_name("terminal-mouse")
                 .long("terminal-mouse")
-                .help("capture mouse events for use with terminal server")
+                .help("TERMINAL: enable capturing of mouse events")
+                .display_order(25)
                 .takes_value(false)
+        )
+
+        .arg(
+            Arg::with_name("messible-address")
+                .long("messible-address")
+                .help("MESSIBLE: address to use to get messible messages from")
+                .display_order(26)
+                .takes_value(true),
         )
 }
 
@@ -306,7 +307,9 @@ fn main() -> Result<(), String> {
         }
     })?;
 
-    bridge.connect().map_err(|e| format!("unable to connect to bridge: {}", e))?;
+    bridge
+        .connect()
+        .map_err(|e| format!("unable to connect to bridge: {}", e))?;
 
     let cfg = Arc::new(cfg);
     let mut threads = vec![];
