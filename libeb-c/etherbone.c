065bc96323e91cbeb98e11ae34d743bc9b2f8093
Andy Kitchen <kitchen.andy+git@gmail.com>
2021-11-28 22:24:45 +1100

    fix TCP reads eb_read32()
    TCP is a streaming protocol so the code that reads
    a TCP socket needs to handle reads that are not a
    multiple of the packet size and buffer them
    correctly. This fixes a bug that cause spurious
    "unexpected read length" errors when using
    a TCP connection.

--- libeb-c/etherbone.c
+++ libeb-c/etherbone.c
@@ -97,11 +97,38 @@ uint32_t eb_read32(struct eb_connection *conn, uint32_t addr) {
 
     eb_send(conn, raw_pkt, sizeof(raw_pkt));
 
-    int count = eb_recv(conn, raw_pkt, sizeof(raw_pkt));
-    if (count != sizeof(raw_pkt)) {
-        fprintf(stderr, "unexpected read length: %d\n", count);
-        return -1;
+    if (conn->is_direct) {
+        int count = eb_recv(conn, raw_pkt, sizeof(raw_pkt));
+
+        if (count != sizeof(raw_pkt)) {
+            fprintf(stderr, "unexpected read length: %d\n", count);
+            return -1;
+        }
+    } else {
+        // If we are connected via TCP we need to take into account any size
+        // read because it is a stream oriented protocol.
+        int ret;
+        uint8_t *p   = raw_pkt;
+        uint8_t *end = raw_pkt + sizeof(raw_pkt);
+
+        while (p < end) {
+            ret = eb_recv(conn, p, end - p);
+
+            if (ret < 0) {
+                switch (errno) {
+                    case EINTR:
+                    case EAGAIN:
+                        continue;
+                    default:
+                        fprintf(stderr, "socket read error: %s\n", strerror(errno));
+                        return -1;
+                }
+            }
+
+            p += ret;
+        }
     }
+
     return eb_unfill_read32(raw_pkt);
 }
 
